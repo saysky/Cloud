dcf03ba1362999eefd75f0f56780d92c4e6b205f
言曌 <847064370@qq.com>
2018-07-18 10:33:24 +0800

    first commit

--- src/main/java/com/liuyanzhao/cloud/controller/UserController.java
+++ src/main/java/com/liuyanzhao/cloud/controller/UserController.java
@@ -3,11 +3,15 @@ package com.liuyanzhao.cloud.controller;
 import com.alibaba.fastjson.JSON;
 import com.liuyanzhao.cloud.entity.User;
 import com.liuyanzhao.cloud.repository.UserRepository;
+import com.liuyanzhao.cloud.vo.ResultVO;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * @author 言曌
@@ -27,70 +31,97 @@ public class UserController {
      * @return
      */
     @GetMapping("/user")
-    public String getUserList() {
-        return JSON.toJSONString(userRepository.findAll());
+    public List<User> getUserList() {
+        return userRepository.findAll();
     }
 
 
     /**
      * 根据ID查询用户
+     *
      * @param id
      * @param response
      * @return
      */
     @GetMapping("/user/{id}")
-    public String getUser(@PathVariable("id") Integer id, HttpServletResponse response) {
+    public ResponseEntity<ResultVO> getUser(@PathVariable("id") Integer id, HttpServletResponse response) {
         User user = userRepository.findOne(id);
         if (user != null) {
-            return JSON.toJSONString(userRepository.findOne(id));
+            HashMap<String,User> map = new HashMap<>();
+            map.put("user",user);
+            return ResponseEntity.ok().body(new ResultVO(0, "操作成功", map));
         } else {
             response.setStatus(404);
-            return JSON.toJSONString("用户不存在!");
+            return ResponseEntity.ok().body(new ResultVO(10001, "用户不存在"));
         }
     }
 
     /**
      * 添加新用户
+     *
      * @param user
      * @param response
      * @return
      */
     @PostMapping("/user")
-    public String saveUser(@RequestBody User user, HttpServletResponse response) {
+    public ResponseEntity<ResultVO> saveUser(@RequestBody User user, HttpServletResponse response) {
+        User result = null;
         try {
-            userRepository.save(user);
+            result = userRepository.save(user);
         } catch (Exception e) {
             response.setStatus(500);
-            return "添加失败！";
+            ResponseEntity.ok().body(new ResultVO(10003, "添加失败！"));
         }
-        return JSON.toJSONString("用户添加成功！");
+        HashMap<String,User> map = new HashMap<>();
+        map.put("user",result);
+        return ResponseEntity.ok().body(new ResultVO(0, "操作成功！", map));
     }
 
     /**
      * 修改用户
+     *
      * @param user
      * @return
      */
-    @PutMapping("/user")
-    public String updateUser(@RequestBody User user) {
-        return JSON.toJSONString(userRepository.save(user));
+    @PutMapping("/user/{id}")
+    public ResponseEntity<ResultVO> updateUser(@PathVariable("id") Integer id, @RequestBody User user, HttpServletResponse response) {
+        User originalUser = userRepository.findOne(id);
+        if(originalUser == null) {
+            response.setStatus(404);
+            return ResponseEntity.ok().body(new ResultVO(10001, "用户不存在"));
+        }
+        User result = null;
+        try {
+            originalUser.setUsername(user.getUsername());
+            originalUser.setPassword(user.getPassword());
+            result = userRepository.save(user);
+        } catch (Exception e) {
+            response.setStatus(500);
+            ResponseEntity.ok().body(new ResultVO(10003, "更新失败！"));
+        }
+        HashMap<String,User> map = new HashMap<>();
+        map.put("user",result);
+        return ResponseEntity.ok().body(new ResultVO(0, "操作成功！", map));
     }
 
     /**
      * 删除用户
+     *
      * @param id
      * @param response
      * @return
      */
     @DeleteMapping("/user/{id}")
-    public String updateUser(@PathVariable("id") Integer id, HttpServletResponse response) {
+    public ResponseEntity<ResultVO> deleteUser(@PathVariable("id") Integer id, HttpServletResponse response) {
         try {
             userRepository.delete(id);
         } catch (Exception e) {
             response.setStatus(404);
-            return JSON.toJSONString("用户不存在！");
+            return ResponseEntity.ok().body(new ResultVO(10001,"用户不存在！"));
         }
-        return JSON.toJSONString("删除成功！");
+        HashMap<String,Integer> map = new HashMap<>();
+        map.put("id",id);
+        return ResponseEntity.ok().body(new ResultVO(0,"请求成功",map));
     }
 
 
